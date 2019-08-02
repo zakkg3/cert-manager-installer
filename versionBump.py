16ce609d3136acedfe0d557d51b1892203be0a96
Nicolas Kowenski <nico@garufa.ethz.ch>
2019-08-02 16:29:39 +0200

    script finished

--- versionBump.py
+++ versionBump.py
@@ -9,7 +9,11 @@ APP_VERSION = os.getenv('APP_VERSION', 'v0.8.1')
 APP_RELEASE = os.getenv('APP_RELEASE', '0.8')
 CHART_VERSION = os.getenv('CHART_VERSION', 'v0.1.0')
 
-print (f'Bumping to version {APP_VERSION}')
+print (f"""Bumping to:
+ Aplication version: {APP_VERSION}
+ Chart version: {CHART_VERSION}
+ Aplication release: {APP_RELEASE}
+ """)
 urllib.request.urlretrieve('https://raw.githubusercontent.com/jetstack/cert-manager/release-'+APP_RELEASE+'/deploy/manifests/00-crds.yaml','./crds-cert-manager/templates/00-crds.yaml')
 
 yaml = YAML()
@@ -22,11 +26,18 @@ chart['version'] = CHART_VERSION
 with open('./crds-cert-manager/Chart.yaml', 'w') as chart_file:
     yaml.dump(chart, chart_file)
 
-# Bump Helmfiles
+# Bump Helmfile CRDS
 with open('./helmfile.d/00-crds.yaml') as helmfile_crds:
     helmfile_crds_data = yaml.load(helmfile_crds)
-helmfile_crds_data[releases][version] = CHART_VERSION
+helmfile_crds_data['releases'][0]['version'] = CHART_VERSION
 with open('./helmfile.d/00-crds.yaml', 'w') as helmfile_crds:
     yaml.dump(helmfile_crds_data, helmfile_crds)
+
+# Bump Helmfile cert-manager
+with open('./helmfile.d/01-cert-manager.yaml') as helmfile_cm:
+    helmfile_cm_data = yaml.load(helmfile_cm)
+helmfile_cm_data['releases'][0]['version'] = APP_VERSION # oficial cert-manager chart version.
+with open('./helmfile.d/01-cert-manager.yaml', 'w') as helmfile_cm:
+    yaml.dump(helmfile_cm_data, helmfile_cm)
 
     
